4918a9b43c6285847323fade065b70341e333d95
EKM96 <124158593+EKM96@users.noreply.github.com>
2023-09-12 14:23:58 -0600

    feat: add functions of basic math operations

---
+++ calculator.js
@@ -0,0 +1,20 @@
+function sum (addend1, addend2) {
+    let sum = addend1 + addend2;
+    return sum;
+};
+
+function substract(minuend, subtrahend) {
+    let difference = minuend - subtrahend;
+    return difference;
+};
+
+function multiply(factor1, factor2) {
+    let product = factor1 * factor2;
+    return product;
+}
+
+function divide(dividend, divisor) {
+    let quotient = dividend / divisor;
+    return quotient;
+}
+
