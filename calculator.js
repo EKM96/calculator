28dcfa6f01652ab8dc4e043047e6e8713a7a3bde
EKM96 <124158593+EKM96@users.noreply.github.com>
2023-09-12 15:05:07 -0600

    feat: add an operate function that takes two operands and an operator and calls one of the basic math functions on the two operands parameters

--- calculator.js
+++ calculator.js
@@ -1,20 +1,48 @@
-function sum (addend1, addend2) {
+let add = function (addend1, addend2) {
     let sum = addend1 + addend2;
     return sum;
 };
 
-function substract(minuend, subtrahend) {
+let substract = function (minuend, subtrahend) {
     let difference = minuend - subtrahend;
     return difference;
 };
 
-function multiply(factor1, factor2) {
+let multiply = function(factor1, factor2) {
     let product = factor1 * factor2;
     return product;
 }
 
-function divide(dividend, divisor) {
+let divide = function(dividend, divisor) {
     let quotient = dividend / divisor;
     return quotient;
 }
 
+let operand1;
+let operand2;
+const operators = [
+    'add',
+    'substract',
+    'multiply',
+    'divide',
+];
+
+function operator(operand1, operand2, operator) {
+    switch (operator) {
+        case 'add':
+            add(operand1, operand2);
+            break;
+        
+        case 'substract': 
+            substract(operand1, operand2);
+            break;
+
+        case 'multiply': 
+            multiply(operand1, operand2);
+            break;
+
+        case 'divide':
+            divide(operand1, operand2);
+            break;
+    }
+}
