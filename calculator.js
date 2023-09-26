a5827742fc4cb2514f7933a80230534a0426d08d
EKM96 <124158593+EKM96@users.noreply.github.com>
2023-09-26 13:23:22 -0600

    revert: remove changes that didn't work as intended
    Refs: 3754684, 6e9e688

--- calculator.js
+++ calculator.js
@@ -7,39 +7,20 @@ let digitsCounter = 0;
 let symbolCounter = 0;
 
 const digitsButtons = document.querySelectorAll('.js-button-digit');
-const expressionContainer = document.querySelector('.js-expression-container');
+const displayContainer = document.querySelector('.js-display');
 const equalButton = document.querySelector('.js-equal-button');
 const operatorsButtons = document.querySelectorAll('.js-operator-button');
-//const regex = /^\d{1,12}[\+\-x\/]\d{1,12}$/;
-const secondaryContainer = document.querySelector('.js-secondary-container');
-
+const regex = /^\d{1,12}[\+\-x\/]\d{1,12}$/;
 
 
 // EVENTS
-/*digitsButtons.forEach((digitButton) => {
-    digitButton.addEventListener('click', displayDigits);
-});*/
 digitsButtons.forEach((digitButton) => {
-    digitButton.addEventListener('click', (digitButton) => {
-        const regex = /^\d{1,10}[\+\-x\/]$/;
-        if(regex.test(expressionContainer.textContent)) {
-            displayInSecondaryContainer();
-            displayDigits(digitButton);
-        } else {
-            displayDigits(digitButton);
-        }
-    })
+    digitButton.addEventListener('click', displayDigits);
 });
 
-function displayInSecondaryContainer() {
-    expression = expressionContainer.textContent;
-    expressionContainer.textContent = '';
-    secondaryContainer.textContent += expression;
-}
-
-/*operatorsButtons.forEach((operatorButton) => {
+operatorsButtons.forEach((operatorButton) => {
     operatorButton.addEventListener('click', () => {
-        if(regex.test(expressionContainer.textContent)) {     
+        if(regex.test(displayContainer.textContent)) {     
             getResult();
             displayOperator(operatorButton); 
         } else {
@@ -47,26 +28,9 @@ function displayInSecondaryContainer() {
         }
     });
 });
-*/
-
-operatorsButtons.forEach((operatorButton) => {
-    operatorButton.addEventListener('click', () => {
-        const regex = /^\d{1,10}[\+\-x\/]$/;
-        if(regex.test(secondaryContainer.textContent)) {     
-            //getResult();
-            //displayOperator(operatorButton); 
-            displayInSecondaryContainer();
-        } else {
-            displayOperator(operatorButton);
-        }
-    });
-});
-
-
 
 equalButton.addEventListener('click', () => {
-    const regex = /^\d{1,12}[\+\-x\/]\d{1,12}$/;
-    if(regex.test(expressionContainer.textContent)) {
+    if(regex.test(displayContainer.textContent)) {
         getResult();
     }
 });
@@ -74,9 +38,9 @@ equalButton.addEventListener('click', () => {
 
 // FUNCTIONS DECLARATIONS
 function displayDigits(event) {
-    if(digitsCounter < 10) {
+    if(digitsCounter < 12) {
         digitDisplayed = event.target.textContent;
-        expressionContainer.textContent += digitDisplayed;
+        displayContainer.textContent += digitDisplayed;
         digitsCounter++;
     } else {
         return;
@@ -85,22 +49,21 @@ function displayDigits(event) {
 
 function displayOperator(operatorButton) {
     let symbol = operatorButton.textContent;
-    expressionContainer.textContent += symbol;
+    displayContainer.textContent += symbol;
     
     symbolCounter++; 
     digitsCounter = 0;
-    dotCounter = 0;
     let isClickedMoreThanOnce = symbolCounter > 1;
 
     if (isClickedMoreThanOnce) {
-        let displayed = expressionContainer.textContent;
-        let displayLength = expressionContainer.textContent.length;
+        let displayed = displayContainer.textContent;
+        let displayLength = displayContainer.textContent.length;
 
-        expressionContainer.textContent = displayed.substring(0, displayLength - 2);
-        expressionContainer.textContent += symbol;
+        displayContainer.textContent = displayed.substring(0, displayLength - 2);
+        displayContainer.textContent += symbol;
     }
 }
-/*
+
 function getResult () { 
     getOperator();
     getOperands();
@@ -109,31 +72,28 @@ function getResult () {
 }
 
 function getOperator() { 
-    expression = expressionContainer.textContent;
+    expression = displayContainer.textContent;
     const operators = /[\+\-x\/]/;
     let operatorIndex = expression.search(operators);
     operator = expression.substring(operatorIndex, operatorIndex + 1);
 }
  
 function getOperands() {
-    expression = expressionContainer.textContent;
+    expression = displayContainer.textContent;
     let operands = expression.split(/[\+\-x\/]/);
     operand1 = +operands[0];
     operand2 = +operands[1];
 };
 
 function removeDisplay() {
-    expressionContainer.textContent = '';
+    displayContainer.textContent = '';
 };
 
 function displayResult() {
         let result = calculate(operator, operand1, operand2);
-        expressionContainer.textContent = result;
+        displayContainer.textContent = result;
         symbolCounter = 0;
 }
-*/
-
-
 
 function calculate(operator, operand1, operand2) {
     switch (operator) {
